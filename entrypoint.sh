2bd8068d6a86ffdd232ba784e03e9f1ab0275a0d
Serebrennikov Stanislav <goodsmileduck@gmail.com>
2021-01-15 16:02:29 +0800

    test12

--- entrypoint.sh
+++ entrypoint.sh
@@ -40,5 +40,5 @@ cd ${SOURCE_DIR}/${CHART_FOLDER}
 /opt/helm package .
 
 /opt/helm dependency update .
-
+/opt/helm version -c
 /opt/helm push ${CHART_FOLDER}-* ${CHARTMUSEUM_URL} -u ${CHARTMUSEUM_USER} -p ${CHARTMUSEUM_PASSWORD} ${FORCE}
